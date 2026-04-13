267d1a2de70a1728d715df0838f7804235f54447
d-merveille <d-m.nzita@ecole-ipssi.net>
2026-04-13 13:17:49 +0200

    Mise à jour navbar responsive

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const navLinks = document.getElementById("navLinks");
 
 burger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+    burger.classList.toggle("active");
 });
